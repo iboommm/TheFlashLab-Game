90dea2815254c26f645dc1c30a12f950ef7b2fbb
Wutthiwat Sornso <iboommm@live.com>
2016-04-02 23:30:08 +0700

    add update

--- core/src/main/java/sut/game01/core/MyGame.java
+++ core/src/main/java/sut/game01/core/MyGame.java
@@ -3,17 +3,21 @@ package sut.game01.core;
 import static playn.core.PlayN.*;
 
 import playn.core.Game;
+import playn.core.Image;
+import playn.core.ImageLayer;
 import playn.core.util.Clock;
 import tripleplay.game.Screen;
 import tripleplay.game.ScreenStack;
 
 public class MyGame extends Game.Default {
-  public static final int UPDATE_RATE = 25;
-  private ScreenStack ss = new ScreenStack();
-  protected final Clock.Source clock = new Clock.Source(UPDATE_RATE);
 
-  public MyGame() {
-    super(UPDATE_RATE);
+    public static final int UPDATE_RATE = 25;
+    private ScreenStack ss = new ScreenStack();
+    protected  final Clock.Source clock = new Clock.Source(UPDATE_RATE);
+
+
+    public MyGame() {
+        super(UPDATE_RATE); // call update every 33ms (30 times per second)
   }
 
   @Override
@@ -23,12 +27,12 @@ public class MyGame extends Game.Default {
 
   @Override
   public void update(int delta) {
-    ss.update(delta);
+      ss.update(delta);
   }
 
   @Override
   public void paint(float alpha) {
     clock.paint(alpha);
-    ss.paint(clock);
+      ss.paint(clock);
   }
 }
